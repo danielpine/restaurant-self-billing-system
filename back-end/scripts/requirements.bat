4e3ba218890addb3f73ac74fff2239f16cc39a46
danielpine <danielpine@sina.com>
2022-03-10 17:00:32 +0800

    import pymysql

--- back-end/scripts/requirements.bat
+++ back-end/scripts/requirements.bat
@@ -4,5 +4,6 @@ pip install flask-sockets
 pip install numpy
 pip install tornado
 pip install pillow
+pip install pymysql
 @REM goto https://www.lfd.uci.edu/~gohlke/pythonlibs/#opencv download opencv for your python version
 @REM pip instal opencv-xxx.whl
