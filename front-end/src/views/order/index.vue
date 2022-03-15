1ce3e15721136c783af1036fd65cb6f939c7da83
danielpine <danielpine@sina.com>
2022-03-15 13:16:32 +0800

    Revert "committed by llf"
    This reverts commit 5ae23c0c49d68f35691f524fd6b58e9c4b47a47d.

--- front-end/src/views/order/index.vue
+++ front-end/src/views/order/index.vue
@@ -1,11 +1,56 @@
 <template>
-  <div class="index-container">
-    Order
-  </div>
+  <a-table
+    :columns="columns"
+    :row-key="(record) => record.key"
+    :data-source="data"
+    :pagination="pagination"
+    :loading="loading"
+    @change="handleTableChange"
+  ></a-table>
 </template>
-
 <script>
+  import { getList } from '@/api/table'
+
   export default {
-    name: 'Index',
+    data() {
+      return {
+        data: [],
+        pagination: {
+          showLessItems: true,
+          showQuickJumper: true,
+          showSizeChanger: true,
+          pageSize: 10,
+          current: 1,
+        },
+        query: {},
+        loading: false,
+        columns:[],
+      }
+    },
+    mounted() {
+      this.fetch()
+    },
+    methods: {
+      handleTableChange(pagination) {
+        const pager = { ...this.pagination }
+        pager.current = pagination.current
+        this.pagination = pager
+        this.fetch()
+      },
+      fetch() {
+        this.loading = true
+        getList({
+          pageSize: this.pagination.pageSize,
+          current: this.pagination.current,
+        }).then(({ data, total,columns }) => {
+          const pagination = { ...this.pagination }
+          pagination.total = total
+          this.loading = false
+          this.data = data
+          this.columns = columns
+          this.pagination = pagination
+        })
+      },
+    },
   }
 </script>
